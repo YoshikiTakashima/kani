451c01f674408289fa73a5a03ca2a5bcf5ec214e
Yoshi Takashima <ytakashl@amazon.com>
2022-08-04 17:36:20 -0400

    Added proptest_compose! test.

--- tests/cargo-kani/proptest/src/lib.rs
+++ tests/cargo-kani/proptest/src/lib.rs
@@ -3,16 +3,19 @@
 
 //! cargo-kani test crate for proptest.
 
-use proptest::test_runner::Config;
+mod proptest_library_is_linked;
 
-// check if the proptest library is linked and macro is working.
-proptest::proptest! {
-    fn successfully_linked_proptest(_ in proptest::strategy::Just(()) ) {
-        let config = Config::default();
-        assert_eq!(
-            config.cases,
-            256,
-            "Default .cases should be 256. Check library/proptest/src/test_runner/config.rs"
-        );
-    }
-}
+// proptest::prop_compose! {
+//   fn nearby_numbers()(centre in -1000..1000)
+//                    (a in centre-10..centre+10,
+//                     b in centre-10..centre+10)
+//                    -> (i32, i32) {
+//     (a, b)
+//   }
+// }
+
+// proptest::proptest! {
+//     fn sum_lower_than_2020((a,b) in nearby_numbers()) {
+//         assert!(a + b < 2020, "each is < 1010, so sum is < 2020");
+//     }
+// }
